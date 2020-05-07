15e4e400f3de697dd37b8ba7ede6de8da72d0417
y162287761 <32408138+y162287761@users.noreply.github.com>
2020-05-07 14:47:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,3 +13,6 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+cd package/lean/
+git clone https://github.com/jerrykuku/lua-maxminddb.git        
+git clone https://github.com/jerrykuku/luci-app-vssr.git
